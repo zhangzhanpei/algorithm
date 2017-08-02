23758b882e65ddd402fbef8a8f8b1eeb3b63166e
zhanpei <zhangzhanpei@gmail.com>
2017-08-02 21:45:42 +0800

    给定一个单链表, 判断是否有环

--- leetcode-141_linkedListCycle.js
+++ leetcode-141_linkedListCycle.js
@@ -1,23 +1,18 @@
-//给定一个单链表, 判断是否为一个循环链表
+//给定一个单链表, 判断是否有环
 //快慢指针是指快指针一次移动两步，慢指针一次移动一步
-//如果是一个环形单链表，那么快指针一定会追上慢指针
-
-function hasCycle(head)
-{
+//如果是一个有环单链表，那么快指针一定会追上慢指针
+var hasCycle = function(head) {
     if (head === null || head.next === null) {
         return false;
     }
-    var slow = head;
-    var fast = head;
-    fast = head.next.next;
-    while (true) {
-        if (fast === null || fast.next === null) {
-            return false;
-        } else if (fast == slow || fast.next == slow) {
+    var slow = head; //慢指针
+    var fast = head; //快指针
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) { //快慢指针相遇
             return true;
-        } else {
-            fast = fast.next.next;
-            slow = slow.next;
         }
     }
-}
+    return false;
+};
