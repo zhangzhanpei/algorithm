1beb1cd6612ebfda00537de13ba5f02e60f2c59e
zhanpei <zhangzhanpei@gmail.com>
2017-10-10 20:07:46 +0800

    找出各部门薪水最高的三位员工

---
+++ leetcode-185_department-top-three-salaries.sql
@@ -0,0 +1,11 @@
+-- 找出各部门薪水最高的三位员工
+SELECT d.Name AS Department, e.Name AS Employee, e.Salary
+FROM Employee e
+JOIN Department d ON e.DepartmentId = d.Id
+WHERE (
+    SELECT COUNT(DISTINCT Salary)
+    FROM Employee
+    WHERE Salary > e.Salary
+        AND DepartmentId = d.Id
+    ) < 3
+ORDER BY d.Name, e.Salary DESC;
