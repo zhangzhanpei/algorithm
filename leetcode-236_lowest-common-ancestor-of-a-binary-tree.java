096e938aab37bb54e8fd21b8157d3f4131a12b95
zhanpei <zhangzhanpei@gmail.com>
2018-03-21 20:21:05 +0800

    给定一个二叉树和两个节点，求两节点的最近公共祖先

---
+++ leetcode-236_lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,15 @@
+/**
+ * 给定一个二叉树和两个节点，求两节点的最近公共祖先
+ */
+public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if (root == null || root == p || root == q) return root;
+    TreeNode left = lowestCommonAncestor(root.left, p, q);
+    TreeNode right = lowestCommonAncestor(root.right, p, q);
+    if (left == null) {
+        return right;
+    } else if (right == null) {
+        return left;
+    } else {
+        return root;
+    }
+}
