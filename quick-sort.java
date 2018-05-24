1fa9a48641fb2915b90e4d0d54bc335d577d58d1
zhanpei <zhangzhanpei@gmail.com>
2018-05-24 23:03:04 +0800

    快速排序

--- quick-sort.java
+++ quick-sort.java
@@ -1,33 +1,37 @@
 /**
  * 快速排序
  */
-public int[] quickSort(int left, int right, int[] arr) {
-    if (left > right) {
-        return null;
+public static void quickSort(int[] nums, int left, int right) {
+    if (left >= right) {
+        return;
     }
+    // 取第一个元素作为比较基数
+    int pivot = nums[left];
     int i = left;
     int j = right;
-    int base = arr[left]; //取第一个元素作为比较
-    while (i != j) {
-        while (arr[j] >= base && i < j) { //从右往左找到第一个小于 base 的元素
+    while (i < j) {
+        // 从右往左找到第一个小于 pivot 的元素
+        while (nums[j] >= pivot && i < j) {
             j--;
         }
-        while (arr[i] <= base && i < j) { //从左往右找到第一个大于 base 的元素
+        // 从左往右找到第一个大于 pivot 的元素
+        while (nums[i] <= pivot && i < j) {
             i++;
         }
-        if (i < j) { //两个元素在基数左右两边时交换两数
-            int tmp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = tmp;
+        // 两个元素在基数左右两边时交换位置
+        if (i < j) {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
         }
     }
 
-    //一趟之后，比基数小的交换到了左边，比基数大的交换到了右边
-    arr[left] = arr[i]; //将基数交换到 i 和 j 相遇的位置
-    arr[i] = base;
+    // 一趟之后，比基数小的交换到了左边，比基数大的交换到了右边
+    // 将基数交换到 i 和 j 相遇的位置
+    nums[left] = nums[i];
+    nums[i] = pivot;
 
     //递归继续处理
-    quickSort(left, i - 1, arr);
-    quickSort(i + 1, right, arr);
-    return arr;
+    quickSort(nums, left, i - 1);
+    quickSort(nums, i + 1, right);
 }
