ab3e6894483829acbb37e14ca2980b736123b7c7
Zhangzhanpei <zhangzhanpei@gmail.com>
2018-08-04 19:08:09 +0800

    查找单链表中间节点

---
+++ leetcode-876_middle-of-the-linked-list.java
@@ -0,0 +1,14 @@
+/**
+ * 查找单链表中间节点(快慢指针)
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
